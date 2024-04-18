93e159fd703a690235fc084fa781f955e66ea9c1
Shubh8195 <shubh300697@gmail.com>
2024-04-18 21:32:50 +0530

    App[Web]: Imported Admin|Inputbox

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import Admin from "@repo/ui/admin";
+import { InputBox } from "@repo/ui/input-box";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Admin />
+      <InputBox />
+    </>
+  );
+};
+
+export default page;
